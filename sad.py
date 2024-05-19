b9e14eb904e63a2f13d61bdd48fd03fd77d5aab3
Tim Oliver <20126140@tafe.wa.edu.au>
2024-05-19 12:20:15 +0800

    feat(blink): sad now blinks, smiley can be arbitrary color

--- sad.py
+++ sad.py
@@ -1,9 +1,11 @@
+import time
 from smiley import Smiley
 
 
 class Sad(Smiley):
+    BLUE = (0, 0, 255)
     def __init__(self):
-        super().__init__()
+        super().__init__(self.BLUE)
 
         self.draw_mouth()
         self.draw_eyes()
@@ -23,4 +25,21 @@ class Sad(Smiley):
         """
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
-            self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+        """
+        Make the sad smiley blink once with a certain delay (in s).
+        This works fine as is, without deriving from the Blinkable abc.
+        When the eyes reopen there are tears at the corner of the eyes.
+        
+        :param delay: Delay in seconds
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        tears = [25, 30, 33, 38]
+        for pixel in tears:
+            self.pixels[pixel] = (120,180,220)
+        self.show()
--- smiley.py
+++ smiley.py
@@ -3,16 +3,18 @@ from sense_hat import SenseHat
 
 class Smiley:
     WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255)
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
     BLANK = (0, 0, 0)
 
-    def __init__(self):
+    def __init__(self, complexion=YELLOW):
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
+        self.my_complexion = complexion
 
-        Y = self.YELLOW
+        Y = self.complexion()
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -24,6 +26,9 @@ class Smiley:
             Y, Y, Y, Y, Y, Y, Y, Y,
             O, Y, Y, Y, Y, Y, Y, O,
         ]
+
+    def complexion(self):
+        return self.my_complexion
 
     def dim_display(self, dimmed=True):
         """
